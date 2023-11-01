cc4095a36c3f4937477ebbeb1e1563b2d2838d19
İSHAK <SHAK@DESKTOP-2GP3AM0>
2023-11-01 23:06:17 +0300

    Add project files.

---
+++ ApiConsume/HotelProject.DataAccessLayer/Concrete/EfRoomDal.cs
@@ -0,0 +1,18 @@
+﻿using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.DataAccessLayer.Repostories;
+using HotelProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DataAccessLayer.Concrete
+{
+    public class EfRoomDal : GenericRepostory<Room>, IRoomDal
+    {
+        public EfRoomDal(Context context) : base(context)
+        {
+        }
+    }
+}
